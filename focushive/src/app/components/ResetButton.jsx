bfa1d1af5713b437bb58627ba72f7c00b98a241a
Fady Kuzman <fady.kuzman@pm.me>
2025-08-25 15:37:06 +0200

    Comment out other modes but focus in the Reset button

--- focushive/src/app/components/ResetButton.jsx
+++ focushive/src/app/components/ResetButton.jsx
@@ -1,16 +1,17 @@
 export default function ResetButton({ resetTimer, isRunning, mode }) {
   // Get style classes based on mode
   const getButtonClasses = (currentMode) => {
-    switch (currentMode) {
-      case 'focus':
-        return 'bg-black-700 text-gray-300 hover:text-gray-600 hover:cursor-pointer border border-gray-600 hover:border-gray-500';
-      case 'shortBreak':
-        return 'bg-green-700 text-green-300 hover:bg-green-800';
-      case 'longBreak':
-        return 'bg-blue-700 text-blue-300 hover:bg-blue-800';
-      default:
-        return 'bg-red-700 text-red-300 hover:bg-red-800';
-    }
+    return "bg-black-700 text-gray-300 hover:text-gray-600 hover:cursor-pointer border border-gray-600 hover:border-gray-500";
+    // switch (currentMode) {
+    //   case 'focus':
+    //     return 'bg-black-700 text-gray-300 hover:text-gray-600 hover:cursor-pointer border border-gray-600 hover:border-gray-500';
+    //   case 'shortBreak':
+    //     return 'bg-green-700 text-green-300 hover:bg-green-800';
+    //   case 'longBreak':
+    //     return 'bg-blue-700 text-blue-300 hover:bg-blue-800';
+    //   default:
+    //     return 'bg-red-700 text-red-300 hover:bg-red-800';
+    // }
   };
 
   return (
