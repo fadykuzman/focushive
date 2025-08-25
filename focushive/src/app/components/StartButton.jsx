71ddfe7908ac4621312878e40ed6e0e526d44486
Fady Kuzman <fady.kuzman@pm.me>
2025-08-25 15:24:13 +0200

    Improve Start Button

--- focushive/src/app/components/StartButton.jsx
+++ focushive/src/app/components/StartButton.jsx
@@ -8,7 +8,7 @@ export default function StartButton({isRunning, startTimer}) {
               ${
                 isRunning
                   ? "bg-red-700 text-red-300 cursor-not-allowed opacity-50"
-                  : "bg-white text-red-500 hover:bg-red-50 shadow-lg hover:shadow-xl transform hover:-translate-y-0.5"
+                  : "bg-black-700 text-red-400 hover:text-red-700 shadow-lg hover:cursor-pointer transform border border-red-400 hover:border-red-300 hover:scale-105"
               }
             `}
     >
